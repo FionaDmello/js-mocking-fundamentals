faa3c10b40763a5332b5c9382362c14cb33ad2ff
FionaDmello <40391218+FionaDmello@users.noreply.github.com>
2024-03-21 12:17:29 +0100

    monkey patch mock of the utils function created for testing

--- src/no-framework/monkey-patching.js
+++ src/no-framework/monkey-patching.js
@@ -1,12 +1,22 @@
-const assert = require('assert')
-const thumbWar = require('../thumb-war')
-const utils = require('../utils')
+// this is an assertion library from node - hover over it to learn more
+const assert = require("assert");
+const thumbWar = require("../thumb-war");
+const utils = require("../utils");
 
-const originalGetWinner = utils.getWinner
-utils.getWinner = (p1, p2) => p1
+let originalGetWinner = utils.getWinner;
+utils.getWinner = (p1, p2) => p1;
 
-const winner = thumbWar('Kent C. Dodds', 'Ken Wheeler')
-assert.strictEqual(winner, 'Kent C. Dodds')
+const winner = thumbWar("Fiona Dmello", "Fiona Shane");
+// because thumbWar uses utils function that randomly picks the winner,
+// the following assertion can pass or fail indeterminately
 
-// cleanup
-utils.getWinner = originalGetWinner
+assert.strictEqual(winner, "Fiona Dmello");
+
+utils.getWinner = originalGetWinner;
+
+// we want to mock the utils function
+// (not use the actual function but a copy of it which may be slightly modified for testing purposes)
+//  we do this as shown in line 20 --> monkey patching
+
+// the important part is to clean up the function and put the right one in place after the testing is done!
+// see lines 19 and 28
