effc909716a59622c874b9edd7c4a69a5e44497b
FionaDmello <40391218+FionaDmello@users.noreply.github.com>
2024-04-10 20:14:43 +0200

    using jest.fn s mock.call object to check for correct params

--- src/__tests__/mock-fn.js
+++ src/__tests__/mock-fn.js
@@ -14,14 +14,18 @@ test("return winner", () => {
   expect(winner2).toBe("A"); //4th call
 
   // below are ways to make sure that mock covers all cases
-  expect(utils.getWinner).toHaveBeenCalledTimes(4);
-  expect(utils.getWinner).toHaveBeenNthCalledWith(
-    1,
-    "Fiona Dmello",
-    "Sharon Salzburg"
-  );
-  expect(utils.getWinner).toHaveBeenNthCalledWith(3, "A", "B");
-  expect(utils.getWinner).toHaveBeenNthCalledWith(4, "A", "B");
+  //expect(utils.getWinner).toHaveBeenCalledTimes(4);
+  //expect(utils.getWinner).toHaveBeenNthCalledWith( 1, "Fiona Dmello", "Sharon Salzburg");
+  //expect(utils.getWinner).toHaveBeenNthCalledWith(3, "A", "B");
+  //expect(utils.getWinner).toHaveBeenNthCalledWith(4, "A", "B");
+
+  // all of the above cases can simply be covered as follows
+  expect(utils.getWinner.mock.calls).toEqual([
+    ["Fiona Dmello", "Sharon Salzburg"],
+    ["Fiona Dmello", "Sharon Salzburg"],
+    ["A", "B"],
+    ["A", "B"],
+  ]);
 
   utils.getWinner = originalGetWinner;
 });
